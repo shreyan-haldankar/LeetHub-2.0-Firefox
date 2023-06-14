cd8985aa8a15d3df4059684b11b7e53a1ee24f84
Shreyan <shreyanhaldankar@gmail.com>
2023-06-14 09:46:11 +0530

    Updated For Firefox Support

--- scripts/oauth2.js
+++ scripts/oauth2.js
@@ -5,12 +5,10 @@ const oAuth2 = {
    */
   init() {
     this.KEY = 'leethub_token';
-    this.ACCESS_TOKEN_URL =
-      'https://github.com/login/oauth/access_token';
-    this.AUTHORIZATION_URL =
-      'https://github.com/login/oauth/authorize';
-    this.CLIENT_ID = '0114dd35b156d4729fac';
-    this.CLIENT_SECRET = 'cfc3301d9745530bf1b31e92528ad9c31fd3f995';
+    this.ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+    this.AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize';
+    this.CLIENT_ID = '';
+    this.CLIENT_SECRET = '';
     this.REDIRECT_URL = 'https://github.com/'; // for example, https://github.com
     this.SCOPES = ['repo'];
   },
